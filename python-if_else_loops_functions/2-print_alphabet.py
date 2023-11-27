ad9bf9d304dcdbad93d387627f7202ea1a976df9
yunusemretokyay1 <yunusemretokyay@gmail.com>
2023-11-27 03:04:44 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        n = chr(i)
-    else:
-        n = chr(i-32)
-    print("{}".format(n), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
