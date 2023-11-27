afa3f3d53b5a2af0fbaa1b7ec35351d3a1bb2a16
yunusemretokyay1 <yunusemretokyay@gmail.com>
2023-11-27 10:09:43 -0800

    1. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,24 +1,4 @@
 #!/usr/bin/python3
-"""A module for square"""
-
-
-class Square:
-    """Represent a square."""
+class Square(object):
     pass
-
-vi 1-square
-
-#!/usr/bin/python3
-"""Square module."""
-
-
-class Square:
-    """Defines a square."""
-
-    def __init__(self, size):
-        """Constructor.
-        Args:
-            size: length of side of the square.
-        """
-        self.__size = size
 
