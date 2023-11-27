885665a33ac0c98ba330d752ea07c5f0809369e0
yunusemretokyay1 <yunusemretokyay@gmail.com>
2023-11-27 13:54:45 -0800

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """
-    Class that defines properties of square by: (based on 1-square.py).
+    """Represent a square."""
 
-    Attributes:
-        size: size of a square (1 side).
-    """
     def __init__(self, size=0):
-        """Creates new instances of square.
+        """Initialize a new Square.
 
         Args:
-            size: size of the square (1 side).
+            size (int): The size of the new square.
         """
-        self.__size = size
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
+        self.__size = size
