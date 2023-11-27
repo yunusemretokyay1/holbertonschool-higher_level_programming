cc6d7073c9aae87dcde079ba10c2987507c3a53c
yunusemretokyay1 <yunusemretokyay@gmail.com>
2023-11-27 14:01:59 -0800

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Square module."""
+# 0-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
+
+
 class Square:
-    """Defines a square."""
+    """Represent a square."""
     pass
